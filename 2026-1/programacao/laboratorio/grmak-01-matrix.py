f022c2772dbec36ac3b7d845de88832fdd8b8698
GUILHERME LOYOLLA RODRIGUES MACHADO <guilherme.machado978@cs.up.edu.br>
2026-04-14 22:00:18 -0300

    Desenhando o início da matrix

--- 2026-1/programacao/laboratorio/grmak-01-matrix.py
+++ 2026-1/programacao/laboratorio/grmak-01-matrix.py
@@ -11,11 +11,27 @@ for i in range(1,8):
         print(f'{i} é ímpar')
 
 
+def estatistica(a:int,b:int,c:int):
+    soma=a+b+c
+    media=soma/3
+    maior=max(a,b,c)
+    menor=min(a,b,c)
+    return soma, media, maior, menor
+
+s, m, mx, mn = estatistica('8',5,10)
+
+print(f'Soma:{s} | Média:{m:.1f} | Maior:{mx} | Menor:{mn}')
+
+
+
+def cor():
+  verdes = ["\033[32m", "\033[38;5;22m", "\033[38;5;28m", "\033[38;5;34m", "\033[38;5;40m", "\033[38;5;46m", "\033[38;5;82m"]
+  return random.randint(0, len(verdes)-1)
 
 
 def fmatrix():
     matrix = ["."*20] * 10
-    verdes = ["\033[32m", "\033[38;5;22m", "\033[38;5;28m", "\033[38;5;34m", "\033[38;5;40m", "\033[38;5;46m", "\033[38;5;82m"]
+    
     reset = "\033[0m"
 
     for i in range(1, len(matrix)):
